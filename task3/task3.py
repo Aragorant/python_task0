ada6dfeddab98cf50e61fbee6ba03c56d3359e64
Aragorant <85631158+Aragorant@users.noreply.github.com>
2021-06-23 15:30:18 +0300

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,6 +1,7 @@
 import http.client
 import tkinter as tk
 from tkinter import *
+import sys
 
 
 conn = http.client.HTTPSConnection("vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com")
@@ -21,7 +22,7 @@ print(val)
 
 
 win = tk.Tk()
-win.geometry(f"600x320+100+200")
+win.geometry(f"600x370+100+200")
 win.title("Task3")
 
 
@@ -151,7 +152,8 @@ def update():
 
 
 
-tk.Button(text='Update', bd=5,font=('Arial', 13), command=update).grid(row=6,column=5, stick='wens', padx=5, pady=5)
+tk.Button(text='Update', bd=5,font=('Arial', 13), command=update).place(x=500, y=320)
+
 
 
 
@@ -186,10 +188,82 @@ label("Italy").grid(column=0, row=4)
 label("Spain").grid(column=0, row=5)
 
 
-label("Click 'Update' to get or update data ").place(x=220, y=270)
+label("Click 'Update' to get or update data ").place(x=220, y=320)
+
+def get():
+    conn = http.client.HTTPSConnection("vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com")
+
+    headers = {
+    'x-rapidapi-key': "a0580e8b98mshde852a61dc71befp1f85d5jsne85be2246ec5",
+    'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+    }
+
+    conn.request("GET", "/api/npm-covid-data/europe", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    val = data.decode("utf-8")
 
 
+    country = ent.get()
+
+    if (val.find(country)) == -1:
+        label('Does\'n exist').grid(column=1, row=6)
+    
+        label('Does\'n exist').grid(column=2, row=6)
+    
+        label('Does\'n exist').grid(column=3, row=6)
+ 
+        label('Does\'n exist').grid(column=4, row=6)
+    
+        label('Does\'n exist').grid(column=5, row=6)
+
+        sys.exit()
 
 
+    def Infection_Risk(country):
+        num = val.find('Infection_Risk',val.find(country))
+        num2 = val.find('Case_Fatality_Rate',val.find(country))
+        return val[num+16:num2-2]
+
+    def Case_Fatality_Rate(country):
+        num = val.find('Case_Fatality_Rate',val.find(country))
+        num2 = val.find('Test_Percentage',val.find(country))
+        return val[num+20:num2-2]
+
+    def Total_Deaths(country):
+        num = val.find('TotalDeaths',val.find(country))
+        num2 = val.find('NewDeaths',val.find(country))
+        return val[num+13:num2-2]
+
+    def Total_Tests(country):
+        num = val.find('TotalTests',val.find(country))
+        num2 = val.find('Population',val.find(country))
+        return val[num+13:num2-3]
+
+    def Population(country):
+        num = val.find('Population',val.find(country))
+        num2 = val.find('one_Caseevery_X_ppl',val.find(country))
+        return val[num+13:num2-3]
+
+    label('                 ').grid(column=1, row=6)
+    label(Infection_Risk(country)).grid(column=1, row=6)
+    
+    label('                 ').grid(column=2, row=6)
+    label(Case_Fatality_Rate(country)).grid(column=2, row=6)
+    
+    label('                 ').grid(column=3, row=6)
+    label(Total_Deaths(country)).grid(column=3, row=6)
+    
+    label('                 ').grid(column=4, row=6)
+    label(Total_Tests(country)).grid(column=4, row=6)
+    
+    label('                 ').grid(column=5, row=6)
+    label(Population(country)).grid(column=5, row=6)
+    
+
+ent = tk.Entry(win, justify=tk.LEFT, font=('Arial', 13), width=2)
+ent.insert(0, '')
+ent.grid(row=6,column=0, stick='we', padx=5)
 
 
+tk.Button(text='Get information', bd=5,font=('Arial', 13), command=get).place(x=10, y=320)
